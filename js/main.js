4589aa7fd6191a3480e7a3621d96c41ed0e9e26e
Josh Sample <joshuawsample@gmail.com>
2017-11-29 01:15:55 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = 'king';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'queen';
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardThree);
